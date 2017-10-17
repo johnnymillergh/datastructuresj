ee98336d1994b18ce532d409154cc95653f39180
Johnny Miller <johnnysviva@outlook.com>
2017-10-17 14:53:09 +0800

    Committed on 10/17/2017

--- Exp06/src/com/jm/exp06/Main.java
+++ Exp06/src/com/jm/exp06/Main.java
@@ -7,12 +7,8 @@ public class Main {
     public static void main(String[] args) {
         HuffmanTreeHelper helper = new HuffmanTreeHelper("E:/Semester Project/201702 Fall Semester/01. Data " +
                 "Structures and Algorithms/02. Experiment/世界名著txt/Alexandre Dumas/The three musketeers.txt");
-        HashMap<Character, Integer> list = new HashMap<>();
-        list.put(new Character('a'), new Integer(1));
-        list.put(new Character('a'), new Integer(2));
-        list.put(new Character('a'), new Integer(3));
-        Character a = 'a';
-        System.out.println(list.get(a));
-        System.out.println(list.size());
+        helper.readFile();
+        helper.count();
+        System.out.println(helper.frequencyList.toString());
     }
 }
