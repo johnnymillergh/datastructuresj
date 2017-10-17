40dbd50a703178d8c49974effcaea3dda4c52704
Johnny Miller <johnnysviva@outlook.com>
2017-10-17 11:44:04 +0800

    Add new constructor.

--- Exp06/src/com/jm/exp06/SequentialHuffmanTree.java
+++ Exp06/src/com/jm/exp06/SequentialHuffmanTree.java
@@ -2,18 +2,23 @@ package com.jm.exp06;
 
 import java.util.Vector;
 
-public class SequentialBinaryTree {
-    private Vector<TreeNode> treeNodes;
+public class SequentialHuffmanTree {
+    private Vector<Node> nodes;
 
-    public SequentialBinaryTree(int initialCapacity, int capacityIncrement) {
-        treeNodes = new Vector<>(initialCapacity, capacityIncrement);
+    public SequentialHuffmanTree(int size) {
+        nodes = new Vector<>();
+        nodes.setSize(2 * size);
     }
 
-    public SequentialBinaryTree(String treeString, int initialCapacity, int capacityIncrement) {
-        treeNodes = new Vector<>(initialCapacity, capacityIncrement);
+    public SequentialHuffmanTree(int initialCapacity, int capacityIncrement) {
+        nodes = new Vector<>(initialCapacity, capacityIncrement);
+    }
+
+    public SequentialHuffmanTree(String treeString, int initialCapacity, int capacityIncrement) {
+        nodes = new Vector<>(initialCapacity, capacityIncrement);
         for (int i = 0; i < treeString.length(); i++) {
             Character character = treeString.charAt(i);
-            treeNodes.addElement(new TreeNode(character));
+            nodes.addElement(new Node(character));
         }
     }
 
@@ -22,7 +27,7 @@ public class SequentialBinaryTree {
         while (true) {
             dataInput = KeyInput.readChar();
             if (dataInput.charValue() != '#') {
-                treeNodes.addElement(new TreeNode(dataInput));
+                nodes.addElement(new Node(dataInput));
             } else {
                 break;
             }
@@ -30,8 +35,8 @@ public class SequentialBinaryTree {
     }
 
     public void display() {
-        for (int i = 0; i < treeNodes.size(); i++) {
-            System.out.print(treeNodes.get(i).getData() + " ");
+        for (int i = 0; i < nodes.size(); i++) {
+            System.out.print(nodes.get(i).getData() + " ");
         }
         System.out.println("<END>");
     }
@@ -43,21 +48,21 @@ public class SequentialBinaryTree {
         SequentialStack stack = new SequentialStack(100);
         stack.push(this.getRootNode());
         while (!stack.isEmpty()) {
-            TreeNode currentRootNode = (TreeNode) stack.pop();
+            Node currentRootNode = (Node) stack.pop();
             visit(currentRootNode);
             // Right child is pushed first so that left is processed first
             int currentIndexOfRightChild = getIndexOfRightChildOf(currentRootNode);
             int currentIndexOfLeftChild = getIndexOfLeftChildOf(currentRootNode);
-            TreeNode currentRightChild;
-            TreeNode currentLeftChild;
+            Node currentRightChild;
+            Node currentLeftChild;
             if (currentIndexOfRightChild > 0) {
-                currentRightChild = treeNodes.get(currentIndexOfRightChild);
+                currentRightChild = nodes.get(currentIndexOfRightChild);
                 if (currentRightChild != null) {
                     stack.push(currentRightChild);
                 }
             }
             if (currentIndexOfLeftChild > 0) {
-                currentLeftChild = treeNodes.get(currentIndexOfLeftChild);
+                currentLeftChild = nodes.get(currentIndexOfLeftChild);
                 if (currentLeftChild != null) {
                     stack.push(currentLeftChild);
                 }
@@ -66,54 +71,54 @@ public class SequentialBinaryTree {
         System.out.println("<END>");
     }
 
-    private void visit(TreeNode node) {
+    private void visit(Node node) {
         System.out.print(node.getData() + " ");
     }
 
-    private int getIndexOfLeftChildOf(TreeNode currentRootNode) {
+    private int getIndexOfLeftChildOf(Node currentRootNode) {
         int indexRoot = indexOf(currentRootNode);
         int indexLeft = indexRoot * 2 + 1;
-        if (indexLeft < treeNodes.size()) {
+        if (indexLeft < nodes.size()) {
             return indexLeft;
         } else {
             return -1;
         }
     }
 
-    private int getIndexOfRightChildOf(TreeNode currentRootNode) {
+    private int getIndexOfRightChildOf(Node currentRootNode) {
         int indexRoot = indexOf(currentRootNode);
         int indexRight = indexRoot * 2 + 2;
-        if (indexRight < treeNodes.size()) {
+        if (indexRight < nodes.size()) {
             return indexRight;
         } else {
             return -1;
         }
     }
 
-    private int indexOf(TreeNode node) {
-        return treeNodes.indexOf(node);
+    private int indexOf(Node node) {
+        return nodes.indexOf(node);
     }
 
     public boolean isEmpty() {
-        return treeNodes.get(0) == null;
+        return nodes.get(0) == null;
     }
 
-    public TreeNode getRootNode() {
+    public Node getRootNode() {
         if (!this.isEmpty()) {
-            return treeNodes.get(0);
+            return nodes.get(0);
         } else {
             return null;
         }
     }
 
-    public Vector<TreeNode> getTreeNodes() {
-        return treeNodes;
+    public Vector<Node> getNodes() {
+        return nodes;
     }
 
-    private class TreeNode {
+    private class Node {
         private Object data;
 
-        public TreeNode(Object data) {
+        public Node(Object data) {
             this.data = data;
         }
 
