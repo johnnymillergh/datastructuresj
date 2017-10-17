f5e5a8210f805a2a75033b106e768501f568b712
Johnny Miller <johnnysviva@outlook.com>
2017-10-17 15:00:28 +0800

    Calculate running time.

--- Exp06/src/com/jm/exp06/Main.java
+++ Exp06/src/com/jm/exp06/Main.java
@@ -5,10 +5,13 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         HuffmanTreeHelper helper = new HuffmanTreeHelper("E:/Semester Project/201702 Fall Semester/01. Data " +
                 "Structures and Algorithms/02. Experiment/世界名著txt/Alexandre Dumas/The three musketeers.txt");
         helper.readFile();
         helper.count();
+        long endTime = System.nanoTime();
+        System.out.println("Time： " + (endTime - startTime) + " ns");
         System.out.println(helper.frequencyList.toString());
     }
 }
